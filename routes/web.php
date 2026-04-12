960c013647e9ca5d0f747a50234aa460ea038cf9
mikhailislami <mikhailislami@students.amikom.ac.id>
2026-04-12 12:16:15 +0700

    tugas 1

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ $data = [
     'nim' => '24.12.3355'
 ];
 
-Route::get('/', fn() => view('home', $data));
+Route::get('/', fn() => view('welcome', $data));
+Route::get('home', fn() => view('home', $data));
 Route::get('/profil', fn() => view('profil', $data));
 Route::get('/katalog', fn() => view('katalog', $data));
 Route::get('/bantuan', fn() => view('bantuan', $data));
