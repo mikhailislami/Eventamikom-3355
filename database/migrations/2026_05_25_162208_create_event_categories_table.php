e48ea94c45cf3f49066f72f9386a202e437a8c9d
mikhailislami <mikhailislami@students.amikom.ac.id>
2026-05-26 12:26:03 +0700

    Update 2026_05_25_162208_create_event_categories_table.php

--- database/migrations/2026_05_25_162208_create_event_categories_table.php
+++ database/migrations/2026_05_25_162208_create_event_categories_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('event_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
     }
